31f5a8bd4325d9ae240e395d5c63a1a453a0cf4b
Jefferson Morales <jefferson2k04@gmail.com>
2026-09-22 12:36:16 -0500

    feat: add class money.ts

--- src/app/shared/domain/model/money.ts
+++ src/app/shared/domain/model/money.ts
@@ -1,2 +1,25 @@
 export class Money {
+  private readonly _amount: number;
+  private readonly _currency: string;
+
+  constructor(amount: number, currency: string = 'PEN') {
+    this._amount = amount;
+    this._currency = currency.toUpperCase();
+  }
+
+  /**
+   * @TODO
+   *
+   * toString(): string {
+   *     return `${this.currency} ${this.amount.toFixed(2)}`;
+   *   }
+   *
+   *   add(other: Money): Money {
+   *     if (this.currency !== other.currency) {
+   *       throw new Error("No se pueden sumar monedas distintas directamente.");
+   *     }
+   *     return new Money(this.amount + other.amount, this.currency);
+   *   }
+   */
+
 }
