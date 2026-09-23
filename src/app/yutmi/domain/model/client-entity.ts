9f72eb7a02fd905452d43851af014c0b2f14dfcc
AAAAA <aaaaaaaaa>
2026-09-22 22:06:30 -0500

    feat: enhance Client class and add ProductEntity class

--- src/app/yutmi/domain/model/client-entity.ts
+++ src/app/yutmi/domain/model/client-entity.ts
@@ -1,12 +1,18 @@
 import  { BaseEntity} from '../../../shared/domain/model/base-entity';
 
 export class Client implements BaseEntity {
-  constructor(props:{id: number, name: string}) {
+  constructor(props:{id: number, name: string, lastName: string, age: number}) {
     this._id = props.id;
     this._name = props.name;
+    this._lastName = props.lastName;
+    this._age = props.age;
+
   }
+
   private _id: number;
   private _name: string;
+  private _lastName: string;
+  private _age: number;
 
   get name(): string { return this._name; }
 
@@ -15,4 +21,12 @@ export class Client implements BaseEntity {
   get id(): number { return this._id; }
 
   set id(value: number) { this._id = value; }
+
+  get lastName(): string { return this._lastName; }
+
+  set lastName(value: string) { this._lastName = value; }
+
+  get age(): number { return this._age; }
+
+  set age(value: number) { this._age = value; }
 }
